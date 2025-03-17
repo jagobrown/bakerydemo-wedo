299d96e5d2bf10b22fd261d6cba7bf77f52fa10c
Jago Brown <brown.jago@gmail.co>
2025-03-17 17:57:13 +0000

    added docker-compose-aws.yml and settings/local.py

---
+++ bakerydemo/settings/local.py
@@ -0,0 +1,18 @@
+# Override settings here
+from .base import *  # noqa: F403, F401
+
+DEBUG = True
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# WAGTAILADMIN_BASE_URL required for notification emails
+# WAGTAILADMIN_BASE_URL = "http://localhost:8000"
+WAGTAILADMIN_BASE_URL = "http://18.168.162.89:8000"
+
+ALLOWED_HOSTS = ["*"]
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
+
